122e1245f8987630ffbc4e197e60c60416a5fecd
KlemenDEV <klemen.pylo@gmail.com>
2021-03-07 22:10:36 +0100

    Initial

---
+++ non_ros/calib_sj4000/calib.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((9*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:7].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,7),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,7), corners2,ret)
+        cv2.imshow('img',img)
+        cv2.waitKey(500)
+
+cv2.destroyAllWindows()
+
+ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+
+# Displayig required output 
+print(" Camera matrix:") 
+print(matrix) 
+  
+print("\n Distortion coefficient:") 
+print(distortion) 
+  
+print("\n Rotation Vectors:") 
+print(r_vecs) 
+  
+print("\n Translation Vectors:") 
+print(t_vecs) 
+
