cdcb6d34686e91d64a26809be9c3a76d32a37900
KlemenDEV <klemen.pylo@gmail.com>
2021-06-01 18:17:30 +0200

    Matlab writer

--- src/visiondrone/script/pose2matlab.py
+++ src/visiondrone/script/pose2matlab.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python
 
 import rospy
-from nav_msgs.msg import Path
+from scipy import io
 from geometry_msgs.msg import PoseStamped
 
+import numpy as np
 
-class OdomToPath:
-    def __init__(self, source, dest):
-        self.odom_sub = rospy.Subscriber(source, PoseStamped, self.odom_cb, queue_size=10)
-        self.path = Path()
+import os
 
-    def odom_cb(self, msg):
-        msg.pose
+data = np.zeros((0, 0))
+
+
+def odom_cb(msg):
+    global data
+    data = np.append(data, np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]))
+
+
+def shutdown():
+    global data
+    path = os.path.abspath(rospy.get_param("~outfile"))
+    rospy.logwarn("Writing matlab file " + path + ", " + str(data.size) + " entries")
+    io.savemat(path, mdict={'exon': data})
 
 
 if __name__ == '__main__':
     rospy.init_node('posetomatlab')
-    odom_to_path = OdomToPath(rospy.get_param("~pose"), rospy.get_param("~path"))
+
+    odom_sub = rospy.Subscriber(rospy.get_param("~pose"), PoseStamped, odom_cb, queue_size=10)
+
+    rospy.on_shutdown(shutdown)
+
     rospy.spin()
