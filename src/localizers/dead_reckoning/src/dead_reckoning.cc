62865e0cc13bf2b87ab3043832e42b98d804f65c
KlemenDEV <klemen.pylo@gmail.com>
2021-09-08 18:13:15 +0200

    Fixed dead reckoning

--- src/localizers/dead_reckoning/src/dead_reckoning.cc
+++ src/localizers/dead_reckoning/src/dead_reckoning.cc
@@ -20,28 +20,27 @@
 #include <thread>
 #include <vector>
 
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-
-typedef message_filters::sync_policies::ApproximateTime <sensor_msgs::Imu, sensor_msgs::Imu> syncPolicy;
-typedef message_filters::Synchronizer <syncPolicy> syncer;
-
 ros::Publisher publisher_velocity;
 
-std::shared_ptr <syncer> syncptr;
-
-Eigen::Vector3d gravity = Eigen::Vector3d(0, 0, 9.81);
 
 double t_last = -1;
-
 int counter = 0;
-
-double roll, pitch, yaw;
+volatile double roll, pitch, yaw;
 
 double vx = 0, vy = 0;
 
-void imuDataCallback(const sensor_msgs::Imu::ConstPtr &imu_msg, const sensor_msgs::Imu::ConstPtr &imu_msg2) {
+void imuDataOrientCallback(const sensor_msgs::Imu::ConstPtr &imu_msg) {
+    tf2::Quaternion quat_est_rot;
+    tf2::fromMsg(imu_msg->orientation, quat_est_rot);
+    tf2::Matrix3x3 est_rot(quat_est_rot);
+    double _roll, _pitch, _yaw;
+    est_rot.getRPY(_roll, _pitch, _yaw);
+    roll = _roll;
+    pitch = _pitch;
+    yaw = _yaw;
+}
+
+void imuDataCallback(const sensor_msgs::Imu::ConstPtr &imu_msg2) {
     if (t_last == -1) {
         t_last = imu_msg2->header.stamp.toSec();
         return;
@@ -49,16 +48,11 @@ void imuDataCallback(const sensor_msgs::Imu::ConstPtr &imu_msg, const sensor_msg
 
     double dt = imu_msg2->header.stamp.toSec() - t_last;
 
-    tf2::Quaternion quat_est_rot;
-    tf2::fromMsg(imu_msg->orientation, quat_est_rot);
-    tf2::Matrix3x3 est_rot(quat_est_rot);
-    est_rot.getRPY(roll, pitch, yaw);
+    double ax = (imu_msg2->linear_acceleration.z) - 9.81 * sin(roll);
+    double ay = (imu_msg2->linear_acceleration.x) - 9.81 * sin(pitch);
 
-    double ax = cos(pitch) * -(imu_msg2->linear_acceleration.z - 0.2566709816455841);
-    double ay = cos(roll) * -(imu_msg2->linear_acceleration.x - 0.03285804018378258);
-
-    vx += ax * dt;
-    vy += ay * dt;
+    vx -= ax * dt * 0.25;
+    vy += ay * dt * 0.25;
 
     if (counter % 10 == 0) {
         geometry_msgs::TwistWithCovarianceStamped velocitymsg;
@@ -70,7 +64,7 @@ void imuDataCallback(const sensor_msgs::Imu::ConstPtr &imu_msg, const sensor_msg
     }
 
     counter++;
-    t_last = imu_msg->header.stamp.toSec();
+    t_last = imu_msg2->header.stamp.toSec();
 }
 
 
@@ -79,13 +73,8 @@ int main(int argc, char **argv) {
 
     ros::NodeHandle nh;
 
-    message_filters::Subscriber <sensor_msgs::Imu> imu1;
-    message_filters::Subscriber <sensor_msgs::Imu> imu2;
-
-    imu1.subscribe(nh, "/imu/9dof", 15);
-    imu2.subscribe(nh, "/camera/imu", 15);
-    syncptr.reset(new syncer(syncPolicy(15), imu1, imu2));
-    syncptr->registerCallback(boost::bind(imuDataCallback, _1, _2));
+    ros::Subscriber sub_imu = nh.subscribe("/imu/9dof", 15, imuDataOrientCallback);
+    ros::Subscriber sub_imu2 = nh.subscribe("/camera/imu", 15, imuDataCallback);
 
     publisher_velocity = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("/dead_reckoning/velocity_out", 1);
 
