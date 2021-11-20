a4d6372c6c722dd5c1f5b633698b95ec018c89ef
KlemenDEV <klemen.pylo@gmail.com>
2021-11-20 14:17:52 +0000

    Update rs ros, fix pfc

--- src/visiondrone/script/pfchecklists.py
+++ src/visiondrone/script/pfchecklists.py
@@ -2,7 +2,7 @@
 import rospy
 import rostopic
 
-from ublox_msgs.msg import NavPVT7
+from ublox_msgs.msg import NavPVT
 
 num_sv = 0
 
@@ -12,31 +12,28 @@ def gps_cb(msg):
     num_sv = msg.numSV
 
 
+def topic_cb(_, topic):
+    active_topics[topic] = True
+
+
 if __name__ == "__main__":
     rospy.init_node('pfchecklist')
-
-    rospy.loginfo("Preflight checks started")
-
-    topics = rospy.get_param("~topics")
-    topics_list = [string.strip() for string in topics.splitlines()]
+    topics = rospy.get_param("~topics").replace('\n', ' ').replace('\r', ' ')
+    topics_list = topics.split()
     active_topics = dict.fromkeys(topics_list, False)
 
-    sub = rospy.Subscriber('/ublox/navpvt', NavPVT7, gps_cb, queue_size=10)
+    sub = rospy.Subscriber('/ublox/navpvt', NavPVT, gps_cb, queue_size=1)
 
-    r = rostopic.ROSTopicHz(-1)
+    for topic in topics_list:
+        rospy.Subscriber(topic, rospy.AnyMsg, topic_cb, queue_size=1, callback_args=topic)
 
-    rate = rospy.Rate(1)  # 1 Hz
+    ros_rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
-        for topic in topics_list:
-            rate = r.get_hz(topic)
-            if rate is not None and rate > 0:
-                active_topics[topic] = True
-
         if all(active_topics.values()):
-            rospy.loginfo("======================================")
-            rospy.loginfo(">>> GPS SV number: %d <<<" % num_sv)
-            rospy.loginfo(">>> PFC DONE, ALL TOPICS ARE READY <<<")
-            rospy.loginfo("======================================")
+            rospy.logerror("######################################")
+            rospy.logerror("#       GPS SV number: %02d            #" % num_sv)
+            rospy.logerror("#   PFC DONE, ALL TOPICS ARE READY   #")
+            rospy.logerror("######################################")
             rospy.signal_shutdown("Preflight checklist done")
 
-        rate.sleep()
+        ros_rate.sleep()
